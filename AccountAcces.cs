c8ed64392ce90e9d406d05e9f6a739dbb6b92730
Grzybekk082 <dawid.grzybkowski@gmail.com>
2024-11-17 17:36:29 +0100

    Dodaj pliki projektów.

---
+++ AccountAcces.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zaliczenie_02._2025
+{
+       internal class AccountAcces
+    {
+        internal string userName, userSurename, userPassword, userLogin;
+        
+
+        
+        AccountAcces(string name, string surename, string password, string login)
+        {
+            userName = name;
+            userSurename = surename;
+            userPassword = password;
+            userLogin = login;
+        }
+        internal string AccountCreator
+        static internal bool LogIn(string login, string password)
+        {
+
+            string directoryPath =Path.Combine( "C:\\Users\\dell\\source\\repos\\Zaliczenie 02.2025\\Accounts");
+            string[] equals = Directory.GetFiles(directoryPath);
+
+            string inputLogin = login,
+                   inputPassword = password,
+                   corectLogin,
+                   corectPassword;
+
+            bool isCorest=false;
+
+            for (int i = 1;i<=equals.Length;i++)
+            {
+                string pathUsers = $"{directoryPath}\\user{i}.txt";
+                StreamReader sr = new StreamReader(pathUsers);
+                corectLogin=sr.ReadLine();
+                corectPassword=sr.ReadLine();
+
+                if(corectLogin.Equals(inputLogin)&&corectPassword.Equals(inputPassword))
+                {
+                    isCorest = true;
+                }
+                else
+                {
+                    if( i==equals.Length)
+                    {
+                        break;
+                    }
+                    continue;
+                }
+            }
+            return isCorest;
+        }
+
+        
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zaliczenie_02._2025
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string login, password;
+
+            Console.WriteLine("SERVICEDESK");
+            Console.WriteLine(@"/-------------------------------------\");
+            Console.WriteLine("Witaj w systemie zarządzania zleceniami");
+            Console.WriteLine(@"/-------------------------------------\");
+            Console.WriteLine("Logowanie");
+            Console.WriteLine();
+
+            Console.Write("Login : ");
+            login = Console.ReadLine();
+            Console.Write("Hasło : ");
+            password = Console.ReadLine();
+            Console.WriteLine();
+            bool isCorect = AccountAcces.LogIn(login, password);
+            if (isCorect)
+            {
+                Console.WriteLine("taki użytkownik istnieje");
+            }
+            else
+            { 
+                Console.WriteLine("Nie ma takiego użytkownika");
+                Console.WriteLine();
+
+                Console.WriteLine(@"/-------------------------------------\");
+                Console.WriteLine($"Nie masz konta? Załóż je, \n " +
+                                    $"administrator serwisu potwierdzi twoje dane.");
+                Console.WriteLine();
+
+                Console.WriteLine("Podaj imię : ");
+                Console.WriteLine("Podaj nazwisko : ");
+
+            }
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
